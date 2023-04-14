7fbfe21a1c96072f51f2b826cf8b858f5f689991
Elise Valloire <elise@lostmechanics.com>
2023-04-14 13:40:11 +0200

    fix bug import Constants

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import {io, Socket} from "socket.io-client";
 import {pinia} from "./main";
 import router from "./router";
 import {useStore} from "./stores/main";
-import {EVENT} from "./common/constants";
+import {EVENT} from "./common/Constants";
 
 
 // TODO : for production
@@ -14,48 +14,48 @@ export const getSocket = () => socket;
 
 export const initClient = () => {
   const store = useStore(pinia);
-  
+
   socket = io(URL, {
     autoConnect: false,
     rejectUnauthorized: false // WARN: please do not do this in production
   });
-  
+
   socket.on("connect", () => {
     store.connected = true;
   });
-  
+
   socket.on("disconnect", () => {
     store.connected = false;
   });
-  
+
   socket.on("join", (arg) => {
     console.log("join", arg);
   });
-  
+
   socket.on(EVENT.LAUNCH_STORY, async (arg) => {
     // TODO :
   });
-  
+
   socket.on(EVENT.START_GAME, async (arg) => {
     await router.push('/game');
   });
-  
+
   socket.on(EVENT.GAME_VALIDATION, async (arg) => {
     // TODO :
   })
-  
+
   socket.on(EVENT.END_GAME, async (arg) => {
     // TODO :
   })
-  
+
   socket.on(EVENT.BACK_STORY, async (arg) => {
     // TODO :
   })
-  
+
   socket.on(EVENT.END_STORY, async (arg) => {
     // TODO :
   })
-  
+
   socket.on(EVENT.SEND_INSTRUCTION, async (arg) => {
     // TODO :
   })
@@ -63,7 +63,7 @@ export const initClient = () => {
 
 export const connectClient = async () => {
   const store = useStore(pinia);
-  
+
   await socket.connect();
   await socket.emit('join', {
     role: store.role,
