e81319c10a61a7edf8b227b2c72972b1a0bc2d07
Maëva Mezzasalma <maeva@nother.io>
2023-06-26 18:27:52 +0200

    Argh

--- src/components/game/teacher/drag-drop/GameView.vue
+++ src/components/game/teacher/drag-drop/GameView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full relative grid grid-cols-12 gap-4 text-center my-auto pt-4"><!-- w-full h-full flex-1 flex items-center  -->
-    <div class="col-span-10 col-start-2 w-full grid grid-cols-3 gap-9 rounded-md p-6 bg-beige-medium"
+    <div class="col-span-10 col-start-2 xl:col-span-8 xl:col-start-3 2xl:col-span-6 2xl:col-start-4 grid grid-cols-3 gap-9 mx-auto rounded-md p-6 bg-beige-medium"
          :class="`grid-cols-${playingTeams.length}`">
       <div v-if="answers" v-for="(team, index) in playingTeams" :v-bind="index"
            class="w-full flex flex-col justify-center items-center gap-2 max-w-[35vh] mx-auto">
